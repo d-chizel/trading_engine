a83f860f966918d9a962b7f02b310bc93c2a9cad
Derrick Chan <derrickkchan@hotmail.com>
2025-09-23 02:41:56 +0800

    m

--- das_lib/utils.py
+++ das_lib/utils.py
@@ -1,5 +1,39 @@
-def format_script(script_type, params):
+class Utils:
     """
     Utility to format scripts for DAS commands.
     """
-    return f"{script_type} {' '.join(params)}\r\n"
+    
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_bid_price(lv1_data):
+        """
+        Extracts the bid price from Level 1 data string.
+        """
+        parts = lv1_data.split()
+        for part in parts:
+            if part.startswith("B:"):
+                try:
+                    return float(part.split(":")[1])
+                except ValueError:
+                    print("Error converting B: price to float.")
+                    return None
+        print("B: price not found in Level 1 data.")
+        return None
+    
+    @staticmethod
+    def get_ask_price(lv1_data):
+        """
+        Extracts the ask price from Level 1 data string.
+        """
+        parts = lv1_data.split()
+        for part in parts:
+            if part.startswith("A:"):
+                try:
+                    return float(part.split(":")[1])
+                except ValueError:
+                    print("Error converting B: price to float.")
+                    return None
+        print("B: price not found in Level 1 data.")
+        return None
--- das_testing.py
+++ das_testing.py
@@ -9,14 +9,13 @@ async def main():
         2. Connect to DAS
         3. Loop through each of the stocks
             3.1. Subscribe to Level 1 data
-            3.2. Wait to receive Level 1 data
-            3.3. Parse the Level 1 data to extract the B: price
-            3.4. Divide 1000 by the B: price from the Level 1 data to get number of shares to sell
-            3.5. Store the information in the dictionary
-            3.6. SLPRICEINQUIRE Short Locate Price Inquire
-            3.7. SLNEWORDER Short Locate New Order
-            3.8. SLOFFEROPERATION Short Locate Offer Operation to accept the lowest price locate offer
-            3.9. NEWORDER to place a market short sale order for the number of shares
+            3.2. Parse the Level 1 data to extract the B: price
+            3.3. Divide 1000 by the B: price from the Level 1 data to get number of shares to sell
+            3.4. Store the information in the dictionary
+            3.5. SLPRICEINQUIRE Short Locate Price Inquire
+            3.6. SLNEWORDER Short Locate New Order
+            3.7. SLOFFEROPERATION Short Locate Offer Operation to accept the lowest price locate offer
+            3.8. NEWORDER to place a market short sale order for the number of shares
         4. #POS Get all positions
         5. Loop through each position
             5.1. If the position is a short position, place a stop loss buy to cover order at 100% above the short sale price
